648be1d853ff512b0ffdb1439a42e6641fbf47a3
GHAFHA <45609663+GHAFHA@users.noreply.github.com>
2022-10-29 12:48:10 -0500

    removed and added files

--- src/components/accountpage.js
+++ src/components/accountpage.js
@@ -3,7 +3,7 @@ import React from "react";
 const Account = () => {
     return (
         <div>
-            <h1>Welcome to the About Page</h1>
+            <h1>Welcome to the Account Page</h1>
         </div>
     )
 }
