775b1b22920e2ceac5d85cf5b8c8afe9d350c4e1
vmm20 <57491580+vmm20@users.noreply.github.com>
2022-11-03 16:33:55 -0700

    Create User class

---
+++ src/components/classes/User.js
@@ -0,0 +1,13 @@
+/*
+    This file created by Vaughan McInerney (vmm190002)
+*/
+
+class User {
+    constructor(userId, institutionalId, displayName) {
+        this.userId = userId;
+        this.institutionalId = institutionalId;
+        this.displayName = displayName;
+    }
+}
+
+export default User;
