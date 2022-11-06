d938121de27637f15b169a3a4860ea962443eea6
vmm20 <57491580+vmm20@users.noreply.github.com>
2022-11-05 19:00:26 -0700

    Add comments and headers to all relevant files

--- src/components/screens/loginpage.js
+++ src/components/screens/loginpage.js
@@ -1,7 +1,17 @@
-/*This file and project was set-up by Noel Johnson nxj190008 */
+/*
+    CS 3354.005 Fall 2022
+    Group 5: Dean Greenberg, Noel Johnson, Oscar Mata-Galvan, Vaughan McInerney, Ruchi Patel, Naod Tesfay
+    Professor Srimathi Srinivasan
+
+    This file created by Noel Johnson (nxj190008)
+
+    This file is part of the user interface.  It generates the Login page in the CometWallet app.
+*/
+
 import React from "react";
 import { Container } from "react-bootstrap";
 
+// import necessary classes
 import LoginController from '../classes/LoginController'
 
 const Login = () => {
