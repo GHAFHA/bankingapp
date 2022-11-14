3e4cef7c0aa2e5adf33de8caad4ba57a389f8495
Nadida22 <116320424+Nadida22@users.noreply.github.com>
2022-11-14 11:43:49 -0600

    Update NotificationPage.js

--- src/components/screens/NotificationPage.js
+++ src/components/screens/NotificationPage.js
@@ -1,75 +1,15 @@
 import React from "react";
 import Accordion from 'react-bootstrap/Accordion';
-<<<<<<< Updated upstream
-import Form from 'react-bootstrap/Form';
-=======
 import Stack from 'react-bootstrap/Stack';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
->>>>>>> Stashed changes
 
 const Settings = () => {
     return (
         <div>
             <Accordion>
                 <Accordion.Item eventKey="0">
-<<<<<<< Updated upstream
-                    <Accordion.Header>Notifications While Login</Accordion.Header>
-                    <Accordion.Body>
-                        <Form>
-                            <Form.Check
-                                type="switch"
-                                label="By Email"
-                                id="custom-switch"
-
-                            />
-                            <Form.Check
-                                type="switch"
-                                label="By Text"
-
-                            />
-                        </Form>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>Notifications While Transactions</Accordion.Header>
-                    <Accordion.Body>
-                        <Form>
-                            <Form.Check
-                                type="switch"
-                                label="By Email"
-                                id="custom-switch"
-
-                            />
-                            <Form.Check
-                                type="switch"
-                                label="By Text"
-
-                            />
-                        </Form>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>Notifications While Low Balance Comet Cash</Accordion.Header>
-                    <Accordion.Body>
-                        <Form>
-                            <Form.Check
-                                type="switch"
-                                label="By Email"
-                                id="custom-switch"
-
-                            />
-                            <Form.Check
-                                type="switch"
-                                label="By Text"
-
-                            />
-                        </Form>
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
-=======
                     <Accordion.Header>Notify Me While Login</Accordion.Header>
                     <Accordion.Body>
                         <Stack direction="horizontal" gap={5}>
@@ -161,7 +101,6 @@ const Settings = () => {
 
             </Accordion>
 
->>>>>>> Stashed changes
         </div>
     )
 }
