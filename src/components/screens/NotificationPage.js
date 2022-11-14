6c11f0d7396a47510861e31f76f5aa333e782127
Nadida22 <116320424+Nadida22@users.noreply.github.com>
2022-11-14 11:40:58 -0600

    Update NotificationPage.js

--- src/components/screens/NotificationPage.js
+++ src/components/screens/NotificationPage.js
@@ -1,21 +1,169 @@
-/*
-    CS 3354.005 Fall 2022
-    Group 5: Dean Greenberg, Noel Johnson, Oscar Mata-Galvan, Vaughan McInerney, Ruchi Patel, Naod Tesfay
-    Professor Srimathi Srinivasan
-
-    This file edited by Naod Tesfay (ndt210000)
-
-    This file is part of the user interface.  It generates the notifications page in the CometWallet app.
-*/
-
 import React from "react";
+import Accordion from 'react-bootstrap/Accordion';
+<<<<<<< Updated upstream
+import Form from 'react-bootstrap/Form';
+=======
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-const Notifications = () => {
+>>>>>>> Stashed changes
+
+const Settings = () => {
     return (
         <div>
-            <h1>Welcome to the Notifications</h1>
+            <Accordion>
+                <Accordion.Item eventKey="0">
+<<<<<<< Updated upstream
+                    <Accordion.Header>Notifications While Login</Accordion.Header>
+                    <Accordion.Body>
+                        <Form>
+                            <Form.Check
+                                type="switch"
+                                label="By Email"
+                                id="custom-switch"
+
+                            />
+                            <Form.Check
+                                type="switch"
+                                label="By Text"
+
+                            />
+                        </Form>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Notifications While Transactions</Accordion.Header>
+                    <Accordion.Body>
+                        <Form>
+                            <Form.Check
+                                type="switch"
+                                label="By Email"
+                                id="custom-switch"
+
+                            />
+                            <Form.Check
+                                type="switch"
+                                label="By Text"
+
+                            />
+                        </Form>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Notifications While Low Balance Comet Cash</Accordion.Header>
+                    <Accordion.Body>
+                        <Form>
+                            <Form.Check
+                                type="switch"
+                                label="By Email"
+                                id="custom-switch"
+
+                            />
+                            <Form.Check
+                                type="switch"
+                                label="By Text"
+
+                            />
+                        </Form>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+=======
+                    <Accordion.Header>Notify Me While Login</Accordion.Header>
+                    <Accordion.Body>
+                        <Stack direction="horizontal" gap={5}>
+                            < span style={{ fontweight: '' }} > By Email </ span>
+                            <Form>
+                                <Form.Check
+                                    type="switch"
+                                    id="custom-switch"
+                                    label="On/Off"
+                                />
+                            </Form>
+                        </Stack>
+
+                        <Stack direction="horizontal" gap={5}>
+                            < span style={{ fontweight: 'bond' }} >  By Text </ span>
+                            <Form>
+                                <Form.Check
+                                    type="switch"
+                                    id="custom-switch"
+                                    label="On/Off"
+                                />
+                            </Form>
+                        </Stack>
+
+                    </Accordion.Body>
+                </Accordion.Item>
+
+            </Accordion>
+
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Notify Me While Transaction</Accordion.Header>
+                    <Accordion.Body>
+                        <Stack direction="horizontal" gap={5}>
+                            < span style={{ fontweight: 'bond' }} > By Email </ span>
+                            <Form>
+                                <Form.Check
+                                    type="switch"
+                                    id="custom-switch"
+                                    label="On/Off"
+                                />
+                            </Form>
+                        </Stack>
+
+                        <Stack direction="horizontal" gap={5}>
+                            < span style={{ fontweight: 'bond' }} >  By Text </ span>
+                            <Form>
+                                <Form.Check
+                                    type="switch"
+                                    id="custom-switch"
+                                    label="On/Off"
+                                />
+                            </Form>
+                        </Stack>
+
+                    </Accordion.Body>
+                </Accordion.Item>
+
+            </Accordion>
+
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Notify Me While Low Balance Comet Cash</Accordion.Header>
+                    <Accordion.Body>
+                        <Stack direction="horizontal" gap={5}>
+                            < span style={{ fontweight: 'bond' }} > By Email </ span>
+                            <Form>
+                                <Form.Check
+                                    type="switch"
+                                    id="custom-switch"
+                                    label="On/Off"
+                                />
+                            </Form>
+                        </Stack>
+
+                        <Stack direction="horizontal" gap={5}>
+                            < span style={{ fontweight: 'bond' }} >  By Text </ span>
+                            <Form>
+                                <Form.Check
+                                    type="switch"
+                                    id="custom-switch"
+                                    label="On/Off"
+                                />
+                            </Form>
+                        </Stack>
+
+                    </Accordion.Body>
+                </Accordion.Item>
+
+            </Accordion>
+
+>>>>>>> Stashed changes
         </div>
     )
 }
 
-export default Notifications;
+export default Settings;
